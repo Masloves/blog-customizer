2a991ee07a6026f4a16bd194dbb9b5b2c9db9b11
Evgeniy Maslov <ruwell.masloves@yandex.ru>
2025-10-02 18:24:32 +0300

    feat(articleparamform, articleparamsform.module, index, radiogroup, option, select): реализация
    Реализовал селекторы изменения стиля, размера и цвета шрифта и функционал применения выбранных
    параметров к статье

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,27 +1,111 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-import { useState } from 'react';
+import { useState, SyntheticEvent } from 'react';
+import { Select } from 'src/ui/select';
+import { Text } from 'src/ui/text';
+import {
+	fontFamilyOptions,
+	OptionType,
+	fontColors,
+	defaultArticleState,
+	ArticleStateType,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+import { RadioGroup } from 'src/ui/radio-group';
+
 import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
-	const [isOpen, setIsOpen] = useState(true);
+export type ArticleParamsFormProps = {
+	onApply: (value: Partial<ArticleStateType>) => void;
+};
+export const ArticleParamsForm = (props: ArticleParamsFormProps) => {
+	const [isFormOpen, setFormIsOpen] = useState(false);
+	const [config, setConfig] =
+		useState<Partial<ArticleStateType>>(defaultArticleState);
+	const { onApply } = props;
+
+	const handleFormIsOpenToggle = () => {
+		setFormIsOpen(!isFormOpen);
+	};
+
+	const handleFontSelect = (option: OptionType) => {
+		setConfig({
+			...config,
+			fontFamilyOption: option,
+		});
+	};
+
+	const handleFontColorSelect = (option: OptionType) => {
+		setConfig({
+			...config,
+			fontColor: option,
+		});
+	};
+
+	const handleFontSizeSelect = (option: OptionType) => {
+		setConfig({
+			...config,
+			fontSizeOption: option,
+		});
+	};
+
+	const handeApplyingChanges = (evt?: SyntheticEvent) => {
+		evt?.preventDefault();
+
+		onApply(config);
+	};
+
+	const handleReset = () => {
+		setConfig(defaultArticleState);
+
+		onApply(config);
+	};
 
 	return (
 		<>
-			<ArrowButton
-				isOpen={isOpen}
-				onClick={() => {
-					setIsOpen(!isOpen);
-				}}
-			/>
+			<ArrowButton isOpen={isFormOpen} onClick={handleFormIsOpenToggle} />
 			<aside
-				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
+				className={clsx(styles.container, {
+					[styles.container_open]: isFormOpen,
+				})}>
 				<form className={styles.form}>
+					<Text as='h2' size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+					<Select
+						title={'шрифт'}
+						placeholder={'Выберите шрифт'}
+						selected={config.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={handleFontSelect}></Select>
+					<RadioGroup
+						name={'размер шрифта'}
+						options={fontSizeOptions}
+						selected={config.fontSizeOption}
+						title={'размер шрифта'}
+						onChange={handleFontSizeSelect}
+					/>
+					<Select
+						title={'цвет шрифта'}
+						placeholder={'Выберите цвет шрифта'}
+						selected={config.fontColor}
+						options={fontColors}
+						onChange={handleFontColorSelect}></Select>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={handleReset}
+						/>
+						<Button
+							title='Применить'
+							htmlType='submit'
+							type='apply'
+							onClick={handeApplyingChanges}
+						/>
 					</div>
 				</form>
 			</aside>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,19 +16,26 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [articleConfig, setArticleConfig] =
+		useState<Partial<ArticleStateType>>(defaultArticleState);
+
+	const handleApplyFormConfig = (value: Partial<ArticleStateType>) => {
+		setArticleConfig(value);
+	};
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': articleConfig.fontFamilyOption?.value,
+					'--font-size': articleConfig.fontSizeOption?.value,
+					'--font-color': articleConfig.fontColor?.value,
+					'--container-width': articleConfig.contentWidth?.value,
+					'--bg-color': articleConfig.backgroundColor?.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm onApply={handleApplyFormConfig}></ArticleParamsForm>
 			<Article />
 		</main>
 	);
